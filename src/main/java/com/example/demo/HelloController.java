6f9a4acb795af0c3ee1134018e6c4314e15ee42a
xiangli2341 <18700362341@163.com>
2018-06-30 13:27:13 +0800

    test111

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/hello") 
+
+public class HelloController {
+    @RequestMapping("/sts")
+    public String helloworld(){  
+        return "使用spring-tool-suite插件";  
+    }  
+}
